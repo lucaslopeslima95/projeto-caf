e8ee2d7167447a3ffc3c5d8590dee02953832a24
lucaslopeslima95 <lucaslopesllima@gmail.com>
2025-02-16 18:11:31 -0300

    Adicionado as rotas para os models criados.

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,35 @@
 <?php
 
-
+use App\Http\Controllers\PerguntaController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\SocioEconomicoController;
+use App\Models\Pessoa;
+use App\Models\Questionario;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/', function () {
     return view('auth.login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
 
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    Route::resources([
+        'usuario'=>ProfileController::class,
+        'pergunta'=>PerguntaController::class,
+        'pessoa'=>Pessoa::class,
+        'questionario'=>Questionario::class
+    ]);
 
-Route::resource('pessoa', SocioEconomicoController::class);
-Route::resource('questionarios', SocioEconomicoController::class);
+
+    
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+    Route::get('/logout', function () {
+        Auth::logout();
+        return redirect('/');
+    })->name('logout');
+});
 
 require __DIR__.'/auth.php';
