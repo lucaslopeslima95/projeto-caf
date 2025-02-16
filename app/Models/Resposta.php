65b58ae5ecf5e1aaa58aad78b727b085c049121b
lucaslopeslima95 <lucaslopesllima@gmail.com>
2025-02-16 16:30:37 -0300

    Adicionado Resposta model.

--- app/Models/Resposta.php
+++ app/Models/Resposta.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Resposta extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'pessoa_id',
+        'pergunta_id',
+        'questionario_id'
+    ];
 }
